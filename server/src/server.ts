4b362d4d2a052af023df6a644c78a77045f2b2ef
Uriel Goncalves <uriel_goncalves@hotmail.com>
2022-11-01 17:21:29 +0000

    Setup ERD gen, change count route, enable cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,36 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
-async function bootstrap(){
+const prisma = new PrismaClient({
+    log: ['query']
+})
+
+async function bootstrap() {
     const fastify = Fastify({
         logger: true, // enables Fastify logging
     })
 
-    // http://localhost:3333/pools
-    fastify.get('/pools/count', () => {
-        return { count: 0 }
+    await fastify.register(cors, {
+        origin: true
     })
 
-    await fastify.listen({ port: 3333})
+    // http://localhost:3333/pools
+    fastify.get('/pools/count', async () => {
+        // const pools = await prisma.pool.findMany({
+        //     where: {
+        //         code: {
+        //             startsWith: 'U'
+        //         }
+        //     }
+        // })
+        // return { pools }
+
+        const count = await prisma.pool.count()
+        return { count }
+    })
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 // starting up
